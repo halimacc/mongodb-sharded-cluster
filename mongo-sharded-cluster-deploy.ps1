67850aa6cb97cb9cba4206ba0e2ce56b9ba1e8a7
Chong He <t-chhe@microsoft.com>
2016-04-22 18:28:37 +0800

    add powershell deploy script

---
+++ mongo-sharded-cluster-deploy.ps1
@@ -0,0 +1,26 @@
+$resourceGroupName="amsc-group-33"
+New-AzureRmResourceGroup -Name $resourceGroupName -Location "East Asia"
+
+$storageAccountPrefix="msctest"
+$configServersSize="Small"
+$shardCount=2;
+$shardSize="XSmall";
+$routerCount=1;
+$adminUsername="azureuser";
+$adminPassword="User@123";
+
+$params=@{
+	storageAccountPrefix=$storageAccountPrefix;
+	configServersSize=$configServersSize;
+	shardCount=$shardCount;
+	shardSize=$shardSize;
+	routerCount=$routerCount;
+	adminUsername=$adminUsername;
+	adminPassword=$adminPassword;
+}
+
+New-AzureRMResourceGroupDeployment -Name DeployMongoShardedCluster -ResourceGroupName $resourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterObject $params
+
+$ipaddr = Get-AzureRmPublicIpAddress -Name router0-pubip -ResourceGroupName $resourceGroupName
+
+echo "router0: $($ipaddr.DnsSettings.Fqdn):27017"
